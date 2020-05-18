0a5e4253798af20a9e2430c20b3c12e059351226
jyoti <jyoti.umarji91@gmail.com>
2020-05-18 16:53:40 +0530

    commit

--- src/test/java/com/terrain/BaseTest.java
+++ src/test/java/com/terrain/BaseTest.java
@@ -1,5 +1,6 @@
 package com.terrain;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -8,29 +9,65 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class BaseTest {
 	
 	static AndroidDriver driver;
 	
-	@Parameters({ "platformName", "platformVersion", "deviceName", "udId", "appPackage", "appActivity" })
-	@BeforeMethod
-	public void beforeMethod(String platformName, String platformVersion, String deviceName, String udId,
-			String appPackage, String appActivity) throws MalformedURLException {
-		
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", platformName);
-		desiredCapabilities.setCapability("platformVersion", platformVersion);
-		desiredCapabilities.setCapability("deviceName", deviceName);
-		desiredCapabilities.setCapability("udId", udId);
-		desiredCapabilities.setCapability("appPackage", appPackage);
-		desiredCapabilities.setCapability("appActivity", appActivity);
+//	@Parameters({ "platformName", "platformVersion", "deviceName", "udId", "appPackage", "appActivity" })
+//	@BeforeMethod
+//	public void beforeMethod(String platformName, String platformVersion, String deviceName, String udId,
+//			String appPackage, String appActivity) throws MalformedURLException {
+//		
+//		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//		desiredCapabilities.setCapability("platformName", platformName);
+//		desiredCapabilities.setCapability("platformVersion", platformVersion);
+//		desiredCapabilities.setCapability("deviceName", deviceName);
+//		desiredCapabilities.setCapability("udId", udId);
+//		desiredCapabilities.setCapability("appPackage", appPackage);
+//		desiredCapabilities.setCapability("appActivity", appActivity);
+//
+//		URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+//
+//		driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//	}
+	
+	
+	
+	@Parameters({ "testName", "accessKey", "platformName", "platformVersion", "deviceName", "udId", "appPackage", "appActivity" })
+    @BeforeMethod
+    public void beforeMethod(String testName, String accessKey, String platformName, String platformVersion, String deviceName,
+            String udId, String appPackage, String appActivity) throws IOException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", platformName);
+        desiredCapabilities.setCapability("platformVersion", platformVersion);
+        desiredCapabilities.setCapability("deviceName", deviceName);
+        desiredCapabilities.setCapability("udId", udId);
+        desiredCapabilities.setCapability("appPackage", appPackage);
+        desiredCapabilities.setCapability("appActivity", appActivity);
 
-		URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+ 
 
-		driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+        if (accessKey.isEmpty()) {
+            URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+
+ 
+
+            driver = new AndroidDriver<MobileElement>(remoteUrl, desiredCapabilities);
+        } else {
+            desiredCapabilities.setCapability("testName", testName);
+            desiredCapabilities.setCapability("accessKey", accessKey);
+            desiredCapabilities.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
+            driver = new AndroidDriver<MobileElement>(new URL("https://demo.experitest.com/wd/hub"), desiredCapabilities);
+
+ 
+
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+          
+    }
 
 }
